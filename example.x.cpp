daf62d2d74b1ff33e9c6b9055f858e4fbaab5161
Nir Friedman <quicknir@gmail.com>
2018-08-16 23:32:37 -0400

    Overwrite original approach with new approach

--- example.x.cpp
+++ example.x.cpp
@@ -1,39 +1,25 @@
-#include "wise_enum.h"
+#include "wise_enum2.h"
 
 #include <iostream>
+#include <cassert>
 
-namespace user {
-WISE_ENUM_CLASS_STORAGE(foo, std::size_t, (bar, 3), baz);
-}
+WISE_ENUM(foo, (bar, 2), baz)
 
 int main() {
-  std::cerr << std::boolalpha;
-  static_assert(sizeof(user::foo) == sizeof(std::size_t), "");
 
-  auto x = user::foo::bar;
-  std::cerr << wise_enum::to_string(x) << " is: " << static_cast<int>(x)
-            << "\n";
-
-  auto y_oops = wise_enum::from_string<user::foo>("babbbb");
-  std::cerr << "incorrect string, optional return: "
-            << static_cast<bool>(y_oops) << "\n";
-  auto y_opt = wise_enum::from_string<user::foo>("bar");
-  std::cerr << "correct string, optional return: " << static_cast<bool>(y_opt)
-            << "\n";
-
-  if (!y_opt)
-    return -1;
-
-  auto y = *y_opt;
-
-  std::cerr << "x and y equal (to bar): " << (y == x) << "\n";
-
-  auto z = user::foo::baz;
-  std::cerr << "z is baz, z and y compare: " << (z == y) << "\n";
-
-  for (const auto &e : wise_enum::descriptor_range<user::foo>) {
-    std::cerr << static_cast<int>(e.first) << ": " << e.second << "\n";
+  for (auto e : wise_enum::descriptor_range<foo>) {
+      std::cerr << e.second;
   }
+  std::cerr << wise_enum::to_string(foo::baz);
+
+  std::cerr << wise_enum::detail::is_wise_enum<foo>;
+  std::cerr << wise_enum::detail::is_wise_enum<int>;
+
+  auto x1 = wise_enum::from_string<foo>("bar");
+  auto x2 = wise_enum::from_string<foo>("barz");
+
+  assert(x1.value() == foo::bar);
+  assert(!x2);
 
   return 0;
 }
--- wise_enum.h
+++ wise_enum.h
@@ -1,116 +1,34 @@
 #pragma once
 
-#include <boost/preprocessor/control/iif.hpp>
-#include <boost/preprocessor/seq/enum.hpp>
-#include <boost/preprocessor/seq/for_each.hpp>
-#include <boost/preprocessor/seq/size.hpp>
-#include <boost/preprocessor/seq/transform.hpp>
-#include <boost/preprocessor/tuple/elem.hpp>
-#include <boost/preprocessor/variadic/to_seq.hpp>
-#include <boost/vmd/is_tuple.hpp>
+#include "wise_enum_detail.h"
+#include "wise_enum_generated.h"
 
 #include <algorithm>
 #include <array>
+#include <type_traits>
 #include <utility>
 
-#include <cstring>
+// optional type needed for interface
+#ifndef WISE_ENUM_IMPL_OPTIONAL
+#if __cplusplus == 201703L
+#include <optional>
+#define WISE_ENUM_IMPL_OPTIONAL std::optional
+#endif
+#endif
 
-#define WISE_ENUM_IMPL_OPTIONAL boost::optional
-#include <boost/optional/optional.hpp>
-
-#define WISE_ENUM_IMPL_XSTR(s) #s
-#define WISE_ENUM_IMPL_STR(s) WISE_ENUM_IMPL_XSTR(s)
-
-#define WISE_ENUM_IMPL_FIRST_OR_ELEM(r, d, enum_value)                         \
-  BOOST_PP_IIF(BOOST_VMD_IS_TUPLE(enum_value),                                 \
-               BOOST_PP_TUPLE_ELEM(0, enum_value), enum_value)
-
-#define WISE_ENUM_IMPL_FIRST_OR_CONC(r, d, enum_value)                         \
-  BOOST_PP_IIF(BOOST_VMD_IS_TUPLE(enum_value),                                 \
-               BOOST_PP_TUPLE_ELEM(0, enum_value) =                            \
-                   BOOST_PP_TUPLE_ELEM(1, enum_value),                         \
-               enum_value)
-
-#define WISE_ENUM_IMPL_ENUM_NAMES(seq)                                         \
-  BOOST_PP_SEQ_TRANSFORM(WISE_ENUM_IMPL_FIRST_OR_ELEM, _, seq)
-
-#define WISE_ENUM_IMPL_SWITCH_TO_STRING(r, enum_name, enum_value)              \
-  case enum_name::enum_value:                                                  \
-    return WISE_ENUM_IMPL_STR(enum_value);
-
-#define WISE_ENUM_IMPL_ENUM_DESC_PAIR(r, enum_name, enum_value)                \
-  std::make_pair(enum_name::enum_value, WISE_ENUM_IMPL_STR(enum_value))
-
-#define WISE_ENUM_IMPL_TO_STRING(name, seq)                                    \
-  constexpr const char *wise_enum_to_string(name e) {                          \
-    switch (e) {                                                               \
-      BOOST_PP_SEQ_FOR_EACH(WISE_ENUM_IMPL_SWITCH_TO_STRING, name, seq)        \
-    }                                                                          \
-  }
-
-#define WISE_ENUM_IMPL_DESC_PAIR_ARRAY(name, seq)                              \
-  constexpr auto wise_enum_descriptor_pair_array(::wise_enum::Tag<name>) {     \
-    return std::array<std::pair<name, const char *>, BOOST_PP_SEQ_SIZE(seq)>{  \
-        BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(                              \
-            WISE_ENUM_IMPL_ENUM_DESC_PAIR, name, seq))};                       \
-  }
-
-#define WISE_ENUM_IMPL_NAME_SEQ(name, enum_names)                              \
-  WISE_ENUM_IMPL_TO_STRING(name, enum_names)                                   \
-  WISE_ENUM_IMPL_DESC_PAIR_ARRAY(name, enum_names)
-
-#define WISE_ENUM_IMPL_SEQ(type, name, storage, seq)                           \
-  type name storage{BOOST_PP_SEQ_ENUM(                                         \
-      BOOST_PP_SEQ_TRANSFORM(WISE_ENUM_IMPL_FIRST_OR_CONC, _, seq))};          \
-  WISE_ENUM_IMPL_NAME_SEQ(name, WISE_ENUM_IMPL_ENUM_NAMES(seq))
-
-#define WISE_ENUM_IMPL(type, name, storage, ...)                               \
-  WISE_ENUM_IMPL_SEQ(type, name, storage, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))
-
-// Interface begins
-#define WISE_ENUM(name, ...)                                                   \
-  WISE_ENUM_IMPL_SEQ(enum, name, , BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))
+// Macro interface
+#define WISE_ENUM(name, ...) WISE_ENUM_IMPL(enum, name, , __VA_ARGS__)
 
 #define WISE_ENUM_STORAGE(name, storage, ...)                                  \
-  WISE_ENUM_IMPL_SEQ(enum, name,                                               \
-                     : storage, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))
+  WISE_ENUM_IMPL(enum, name, : storage, __VA_ARGS__)
 
 #define WISE_ENUM_CLASS(name, ...)                                             \
-  WISE_ENUM_IMPL_SEQ(enum class, name, , BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))
+  WISE_ENUM_IMPL(enum class, name, , __VA_ARGS__)
 
 #define WISE_ENUM_CLASS_STORAGE(name, storage, ...)                            \
-  WISE_ENUM_IMPL_SEQ(enum class, name,                                         \
-                     : storage, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))
+  WISE_ENUM_IMPL(enum class, name, : storage, __VA_ARGS__)
 
 namespace wise_enum {
-namespace detail {
-
-template <class T, std::size_t N>
-constexpr std::array<T, N>
-desc_array_to_array(const std::array<std::pair<T, const char *>, N> &a) {
-  std::array<T, N> r;
-  for (std::size_t i = 0; i != N; ++i) {
-    r[i] = a[i].first;
-  }
-  return r;
-}
-
-constexpr int strcmp(const char *s1, const char *s2) {
-  while (*s1 && (*s1 == *s2))
-    s1++, s2++;
-  if (*s1 < *s2) {
-    return -1;
-  }
-  if (*s1 > *s2) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-}
-
-template <class T>
-struct Tag {};
 
 template <class T>
 constexpr const char *to_string(T t) {
@@ -118,7 +36,8 @@ constexpr const char *to_string(T t) {
 }
 
 template <class T>
-constexpr auto descriptor_range = wise_enum_descriptor_pair_array(Tag<T>{});
+constexpr auto
+    descriptor_range = wise_enum_descriptor_pair_array(detail::Tag<T>{});
 
 template <class T>
 constexpr std::size_t size = descriptor_range<T>.size();
@@ -127,7 +46,13 @@ template <class T>
 constexpr auto range = detail::desc_array_to_array(descriptor_range<T>);
 
 template <class T>
-constexpr WISE_ENUM_IMPL_OPTIONAL<T> from_string(const char *arg) {
+static constexpr bool is_wise_enum_v = detail::is_wise_enum<T>;
+
+template <class T>
+struct is_wise_enum : std::integral_constant<bool, is_wise_enum_v<T>> {};
+
+template <class T>
+constexpr std::optional<T> from_string(const char *arg) {
   auto it =
       std::find_if(descriptor_range<T>.begin(), descriptor_range<T>.end(),
                    [=](const auto &x) {
