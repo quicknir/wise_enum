a564ffc525468a10d6b096d42698a5b98a28fcd2
Nir Friedman <quicknir@gmail.com>
2018-08-16 23:47:01 -0400

    Improvements to example

--- example.x.cpp
+++ example.x.cpp
@@ -1,25 +1,41 @@
-#include "wise_enum2.h"
+#include "wise_enum.h"
 
-#include <iostream>
 #include <cassert>
+#include <iostream>
 
 WISE_ENUM(foo, (bar, 2), baz)
 
 int main() {
 
-  for (auto e : wise_enum::descriptor_range<foo>) {
-      std::cerr << e.second;
+  // Number of enumerations:
+  std::cerr << "Number of enumerations: " << wise_enum::size<foo> << "\n";
+
+  // Iterate over the enums:
+  std::cerr << "Enum values:";
+  for (auto e : wise_enum::range<foo>) {
+    std::cerr << " " << static_cast<int>(e);
   }
-  std::cerr << wise_enum::to_string(foo::baz);
+  std::cerr << "\n";
 
-  std::cerr << wise_enum::detail::is_wise_enum<foo>;
-  std::cerr << wise_enum::detail::is_wise_enum<int>;
+  // Iterate with descriptors
+  std::cerr << "Enum values and descriptors:\n";
+  for (auto e : wise_enum::descriptor_range<foo>) {
+    std::cerr << static_cast<int>(e.first) << " " << e.second << "\n";
+  }
+  std::cerr << "\n";
 
+  // Convert any enum to a string
+  std::cerr << wise_enum::to_string(foo::baz) << "\n";
+
+  // Convert any string to an optional enum
   auto x1 = wise_enum::from_string<foo>("bar");
   auto x2 = wise_enum::from_string<foo>("barz");
 
   assert(x1.value() == foo::bar);
   assert(!x2);
+
+  std::cerr << wise_enum::detail::is_wise_enum<foo>;
+  std::cerr << wise_enum::detail::is_wise_enum<int>;
 
   return 0;
 }
