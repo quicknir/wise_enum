fee150c7cc03de4f7fdff2192f65c7fa56a3bdde
Nir Friedman <quicknir@gmail.com>
2018-08-17 22:05:09 -0400

    Change names of enumeration/enumerators in example

--- example.x.cpp
+++ example.x.cpp
@@ -8,42 +8,44 @@
 #include <cassert>
 #include <iostream>
 
-WISE_ENUM(foo, (bar, 2), baz)
+// Equivalent to enum Color {GREEN = 2, RED};
+WISE_ENUM(Color, (GREEN, 2), RED)
 
 int main() {
 
   // Number of enumerations:
-  std::cerr << "Number of enumerators: " << wise_enum::size<foo> << "\n";
+  static_assert(wise_enum::size<Color> == 2, "");
+  std::cerr << "Number of enumerators: " << wise_enum::size<Color> << "\n";
 
   // Iterate over the enums:
   std::cerr << "Enum values:";
-  for (auto e : wise_enum::range<foo>) {
+  for (auto e : wise_enum::range<Color>) {
     std::cerr << " " << static_cast<int>(e);
   }
   std::cerr << "\n";
 
   // Iterate with descriptors
   std::cerr << "Enum values and descriptors:\n";
-  for (auto e : wise_enum::descriptor_range<foo>) {
+  for (auto e : wise_enum::descriptor_range<Color>) {
     std::cerr << static_cast<int>(e.first) << " " << e.second << "\n";
   }
   std::cerr << "\n";
 
   // Convert any enum to a string
-  std::cerr << wise_enum::to_string(foo::baz) << "\n";
+  std::cerr << wise_enum::to_string(Color::RED) << "\n";
 
   // Convert any string to an optional enum
-  auto x1 = wise_enum::from_string<foo>("bar");
-  auto x2 = wise_enum::from_string<foo>("barz");
+  auto x1 = wise_enum::from_string<Color>("GREEN");
+  auto x2 = wise_enum::from_string<Color>("Greeeeeeen");
 
-  assert(x1.value() == foo::bar);
+  assert(x1.value() == Color::GREEN);
   assert(!x2);
 
   // Everything is constexpr, and a type trait is made available for easy use in
   // enable_if/tag dispatch
-  static_assert(wise_enum::is_wise_enum_v<foo>, "");
+  static_assert(wise_enum::is_wise_enum_v<Color>, "");
   static_assert(!wise_enum::is_wise_enum_v<int>, "");
-  enum flub { blub, glub};
+  enum flub { blub, glub };
   static_assert(!wise_enum::is_wise_enum_v<flub>, "");
 
   return 0;
